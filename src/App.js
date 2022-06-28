5a4892bd582780ce790f3587bf5ba247cc9a3738
pixelchai <30553505+pixelchai@users.noreply.github.com>
2022-06-28 14:34:00 +0100

    work on setting up selecting

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "./App.css";
 import pokemon from "./pokemon.json";
 
@@ -9,6 +10,9 @@ const PokemonRow = ({ pokemon }) => (
 );
 
 function App() {
+  const [searchValue, searchValueSet] = React.useState("");
+  const [selectedItem, selectedItemSet] = React.useState(null);
+
   return (
     <div
       style={{
@@ -18,20 +22,40 @@ function App() {
       }}
     >
       <h1 className="title">Pokemon Search</h1>
-      <input />
-      <table width="100%">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Type</th>
-          </tr>
-        </thead>
-        <tbody>
-          {pokemon.slice(0, 20).map((pokemon) => (
-            <PokemonRow pokemon={pokemon} key={pokemon.id} />
-          ))}
-        </tbody>
-      </table>
+      <input
+        value={searchValue}
+        onChange={(e) => searchValueSet(e.target.value)}
+      />
+
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "70% 30%",
+          gridColumnGap: "1rem",
+        }}
+      >
+        <table width="100%">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Type</th>
+            </tr>
+          </thead>
+          <tbody>
+            {pokemon
+              .filter((pokemon) =>
+                pokemon.name.english
+                  .toLowerCase()
+                  .includes(searchValue.toLocaleLowerCase())
+              )
+              .slice(0, 20)
+              .map((pokemon) => (
+                <PokemonRow pokemon={pokemon} key={pokemon.id} />
+              ))}
+          </tbody>
+        </table>
+      </div>
+      {selectedItem && <h1>selectedItem.name.english</h1>}
     </div>
   );
 }
